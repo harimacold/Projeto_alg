f7dd9ed3fc3c2b0f31fd26bb8f36ed7b5e1f0c3d
CRISTOPHER LUIS MATTOSO <clm15@inf.ufpr.br>
2015-11-03 11:44:23 -0200

    lteradas bibliotecas e projeto.c

--- projeto.c
+++ projeto.c
@@ -2,47 +2,36 @@
 #include "pilhas.h"
 #include "listas.h"
 
-
-typedef struct tp_recibo{
-  char cliente[50];
-  float total;
-} recibo;
-
-float valor_total(lista *lis)
-{
-  float total = 0;
-  nodo_lis *aux;
-  if (Lista_vazia(*lis))
-    printf ("Lista vazia");
-  else
-    for (aux = lis->prox; aux != lis; aux = aux->prox)
-      total += lis->elem.valor;
-  return total;
-}
-
-void cadastra_pessoa(fila *F){
-	pessoa cliente;
-	scanf("\n%[^\n]",cliente.nome);
-	while(cliente.nome !="0"){
-		cadastra_lp(&cliente.lista_LP);	
-		enfileirar(F, cliente);
-		scanf("\n%[^\n]",cliente.nome);
-	}
-}
 void cadastra_lp(lista *lps){
 	LP lp;
 	inicLista(lps);
+	printf ("Nome LP: ");
 	scanf("\n%[^\n]",lp.nome);
-	while (lp.nome !="0"){
-	scanf("\n%[^\n]",lp.album);
-	scanf("%f",lp.valor);
-	insereLista(lps,lp);
-	scanf("\n%[^\n]",lp.nome);
+	while (lp.nome[0] != '0'){
+	  printf ("Nome Album: ");
+	  scanf("\n%[^\n]",lp.album);
+	  printf ("Valor: ");
+	  scanf("%f",&(lp.valor));
+	  insereLista(lps,lp);
+	  printf ("Nome LP: ");
+	  scanf("\n%[^\n]",lp.nome);
 	}
 }
+void cadastra_pessoa(fila *F){
+	pessoa cliente;
+	printf ("Nome Pessoa: ");
+	scanf("\n%[^\n]",cliente.nome);
+	while(cliente.nome[0] != '0'){
+		cadastra_lp(&cliente.lista_LP);	
+		enfileirar(F, cliente);
+		printf ("Nome Pessoa: ");
+		scanf("\n%[^\n]",cliente.nome);
+	}
+}
+
 int main(void){
 	fila pessoas;
-	cadastra(&pessoas);
+	cadastra_pessoa(&pessoas);
 	imprimeFila(pessoas);
 }
   
